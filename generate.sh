7dbbf3a6e02df7ee523bfe2af0d946accaf5bebe
thisconnect <p7tryk@gmail.com>
2020-06-13 19:09:11 +0200

    cleanup

--- generate.sh
+++ generate.sh
@@ -5,20 +5,20 @@ rm *.csv
 echo "'prawdopodobienstwo','stopandwait','gobackn'" >> procent.csv
 for i in $(seq 100 -1 10)
 do
-    output=$(./arq 1000 $i 30 5)
+    output=$(./arq.out 1000 $i 30 5)
     echo "$i, $output" >> procent.csv
 done
 
 echo "'ping','stopandwait','gobackn'" >> ping.csv
 for i in $(seq 30 1 200)
 do
-    output=$(./arq 1000 95 $i 5)
+    output=$(./arq.out 1000 95 $i 5)
     echo "$i, $output" >> ping.csv 
 done
 
 echo "'wielkosc ramki','stopandwait','gobackn'" >> ramka.csv
 for i in $(seq 1 1 30)
 do
-    output=$(./arq 1000 95 30 $i)
+    output=$(./arq.out 1000 95 30 $i)
     echo "$i, $output" >> ramka.csv
 done
